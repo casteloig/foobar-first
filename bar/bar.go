f01a1271ff8eafa59b2158877633f3d295a5595c
Ignacio Castelo <nacho.castelo@gmail.com>
2021-06-05 21:49:22 +0200

    Added simple structured logs using Logrus

--- bar/bar.go
+++ bar/bar.go
@@ -7,9 +7,35 @@ import (
 	"os"
 
 	pb "bar.proy1/proto"
-
 	"google.golang.org/grpc"
+
+	"github.com/sirupsen/logrus"
 )
+
+var listeningLog = logrus.New()
+var templateLogger *logrus.Entry
+
+func createTemplateLogger(ip string, port string) {
+	templateLogger = listeningLog.WithFields(logrus.Fields{
+		"ip":   ip,
+		"port": port,
+	})
+}
+
+func setupStructLogging() {
+
+	var filename string = "logs_bar.log"
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+
+	if err != nil {
+		log.Println(err)
+	} else {
+		listeningLog.Out = f
+	}
+	listeningLog.Formatter = &logrus.JSONFormatter{}
+	listeningLog.Level = logrus.TraceLevel
+
+}
 
 type server struct {
 	pb.UnimplementedBarServiceServer
@@ -21,6 +47,8 @@ func (s *server) BarFunc(ctx context.Context, in *pb.Request) (*pb.Response, err
 
 func main() {
 
+	setupStructLogging()
+
 	ip, err := os.LookupEnv("LIS_IP")
 	if !err {
 		ip = "0.0.0.0"
@@ -30,17 +58,20 @@ func main() {
 	if !err {
 		port = "4001"
 	}
+	createTemplateLogger(ip, port)
 
-	log.Printf("server listening at %v %v", ip, port)
+	templateLogger.Info("Server listening")
 	lis, error := net.Listen("tcp", ip+":"+port)
 	if error != nil {
-		log.Fatalf("failed to listen: %v", err)
+		templateLogger.Fatal("Failed to listen")
 	}
 
 	s := grpc.NewServer()
 	pb.RegisterBarServiceServer(s, &server{})
-	log.Printf("server listening at %v", lis.Addr())
+
+	templateLogger.Info("Serving at ")
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		templateLogger.Fatal("failed to serve")
 	}
+
 }
--- bar/go.mod
+++ bar/go.mod
@@ -3,6 +3,7 @@ module bar.proy1
 go 1.16
 
 require (
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
 	golang.org/x/sys v0.0.0-20210601080250-7ecdf8ef093b // indirect
 	google.golang.org/genproto v0.0.0-20210601170153-0befbe3492e2 // indirect
