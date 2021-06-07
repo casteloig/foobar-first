7d26e7f7f6e66085f260992e6498a58cf9b77151
Ignacio Castelo <nacho.castelo@gmail.com>
2021-06-08 01:41:56 +0200

    Added simple structured logs on foo (python) using python-json-logger

--- foo/foo.py
+++ foo/foo.py
@@ -9,7 +9,8 @@ sys.path.append('./proto')
 import service_pb2 as pb2
 import service_pb2_grpc as pb2_grpc
 
-
+import logging
+from pythonjsonlogger import jsonlogger
 
 
 app = flask.Flask(__name__)
@@ -19,20 +20,31 @@ ip = os.getenv('LIS_IP', "0.0.0.0")
 port = os.getenv('LIS_PORT', "4000")
 bar_endpoint = os.getenv('BAR_ENDPOINT', "bar:4001")
 
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+handler = logging.FileHandler('logs_foo.log', mode='a')
+
+formatter = jsonlogger.JsonFormatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+
+log.addHandler(handler)
+
+#logger.removeHandler(handler)
+#handler.close()
+
+log.info('Initialized')
+
 @app.route('/foo', methods=['GET'])
 def home():
-
- 
+    
+    log.info('Listening')
     with grpc.insecure_channel(bar_endpoint) as channel:
         stub = pb2_grpc.BarServiceStub(channel)
         response = stub.BarFunc(pb2.Request(a = True))
-    return "foo" + response.result
 
+    return "foo" + response.result
 
 
 app.run(host=ip, port=port)
 
-#import logging
-
-#logging.basicConfig(filename='foo.log', filemode='x+a', format='%(name)s - %(levelname)s - %(message)s')
-# logging.info('Sending gRPC request to ' + bar_endpoint)
