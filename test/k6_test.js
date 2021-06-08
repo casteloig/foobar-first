09b7690ff7226a65c38481e8b735899f9626682d
Ignacio Castelo <nacho.castelo@gmail.com>
2021-06-08 03:31:15 +0200

    Added k6 simple checking test

---
+++ test/k6_test.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { sleep, check} from 'k6';
+
+export let options = {
+    vus: 5,
+    duration: '1m'
+};
+
+const BASE_URL = 'http://localhost:4000/foo';
+
+
+export default function () {
+    let res = http.get(`${BASE_URL}`);
+    check(res, {
+        'is response "foobar"': (res) => res.body == 'foobar'
+    });
+    sleep(1);
+}
