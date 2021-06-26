d302ad04d614c708dd9a14c0c7d8a044f3681fc9
Ignacio Castelo <nacho.castelo@gmail.com>
2021-06-27 01:04:54 +0200

    Finished wowtoken script

--- scripts/wowtoken.py
+++ scripts/wowtoken.py
@@ -1,23 +1,61 @@
-from typing import Type
 import requests
-from requests import HTTPError
-import sys
-import os
-
+import argparse
 
 auth_uri = "https://us.battle.net/oauth/token"
-client_id = os.getenv("CLIENT_ID", "")
-client_secret = os.getenv("CLIENT_SECRET", "")
 
 
-def auth():
+def auth(client_id, client_secret):
+    """
+    Retrieves the bearer token of Battle.net using client_id and client_secret
+
+    Parameters
+    ----------
+        client_id : str
+            client_id of battlenet
+        client_secret: str
+            client_secret of the client_id
+
+    Returns
+    -------
+        str -> the bearer token
+    """
+
     payload = {"grant_type": "client_credentials"}
-    response = requests.post(auth_uri, auth=(client_id, client_secret), data=payload)
+    try:
+        response = requests.post(
+            auth_uri, auth=(client_id, client_secret), data=payload
+        )
+    except requests.exceptions.RequestException as e:
+        print(f"Request error")
+        raise SystemExit(e)
 
-    return response.json()["access_token"]
+    try:
+        response = response.json()["access_token"]
+    except LookupError as e:
+        print(f"Token cloud not be decoded")
+        raise SystemExit(e)
+
+    return response
 
 
 def getPrice(region, bearer):
+    """
+    Retrieves price (gold, silver, bronze) of the region's token
+
+    Parameters
+    ----------
+        region: str
+            Region whose token's price we want to know
+        bearer: str
+            bearer token to get the information
+
+    Returns
+    -------
+        str -> gold cost
+        str -> silver cost
+        str -> bronze cost
+    """
+
     uri = f"https://{region}.api.blizzard.com/data/wow/token/index?"
     namespace = f"namespace=dynamic-{region}"
 
@@ -28,16 +66,36 @@ def getPrice(region, bearer):
     header = {}
     header["Authorization"] = f"Bearer {bearer}"
 
-    response = requests.get(uri, headers=header)
+    try:
+        response = requests.get(uri, headers=header)
+    except requests.exceptions.RequestException as e:
+        print("Request error")
+        raise SystemExit(e)
 
-    print(response.json()["price"])
+    priceString = str(response.json()["price"])
+    bronze = priceString[len(priceString) - 2 : len(priceString)]
+    silver = priceString[len(priceString) - 4 : len(priceString) - 2]
+    gold = priceString[0 : len(priceString) - 4]
+
+    return gold, silver, bronze
 
 
 def main():
-    bearer = auth()
-    getPrice("us", bearer)
-    getPrice("eu", bearer)
-    getPrice("cn", bearer)
+
+    parser = argparse.ArgumentParser(
+        description="Retrieve the token's price of all regions"
+    )
+    parser.add_argument("--id", type=str, help="Client ID of Blizzard's API")
+    parser.add_argument("--secret", type=str, help="Client ID of Blizzard's API")
+    args = parser.parse_args()
+
+    client_id = args.id
+    client_secret = args.secret
+
+    bearer = auth(client_id, client_secret)
+    for i in ["us", "eu", "kr", "tw", "cn"]:
+        gold, silver, bronze = getPrice(i, bearer)
+        print(f"Region = {i} -> gold: {gold}, silver: {silver}, bronze: {bronze}")
 
 
 if __name__ == "__main__":
